8304370f79a8bf202105181f77952decd0559639
Adam PC <xadirpl@gmail.com>
2026-04-13 19:16:45 +0200

    readded weighted agreggate

--- VARS_model/mvaggregate.py
+++ VARS_model/mvaggregate.py
@@ -3,6 +3,63 @@ import torch
 from torch import nn
 
 
+class WeightedAggregate(nn.Module):
+    def __init__(self,  model, feat_dim, lifting_net=nn.Sequential()):
+        super().__init__()
+        self.model = model
+        self.lifting_net = lifting_net
+        num_heads = 8
+        self.feature_dim = feat_dim
+
+        r1 = -1
+        r2 = 1
+        self.attention_weights = nn.Parameter((r1 - r2) * torch.rand(feat_dim, feat_dim) + r2)
+
+        self.normReLu = nn.Sequential(
+            nn.LayerNorm(feat_dim),
+            nn.ReLU()
+        )
+
+        self.relu = nn.ReLU()
+
+
+
+    def forward(self, mvimages):
+        B, V, C, D, H, W = mvimages.shape # Batch, Views, Channel, Depth, Height, Width
+        aux = self.lifting_net(unbatch_tensor(self.model(batch_tensor(mvimages, dim=1, squeeze=True)), B, dim=1, unsqueeze=True))
+
+
+        ##################### VIEW ATTENTION #####################
+
+        # S = source length
+        # N = batch size
+        # E = embedding dimension
+        # L = target length
+
+        aux = torch.matmul(aux, self.attention_weights)
+        # Dimension S, E for two views (2,512)
+
+        # Dimension N, S, E
+        aux_t = aux.permute(0, 2, 1)
+
+        prod = torch.bmm(aux, aux_t)
+        relu_res = self.relu(prod)
+
+        aux_sum = torch.sum(torch.reshape(relu_res, (B, V*V)).T, dim=0).unsqueeze(0)
+        final_attention_weights = torch.div(torch.reshape(relu_res, (B, V*V)).T, aux_sum.squeeze(0))
+        final_attention_weights = final_attention_weights.T
+
+        final_attention_weights = torch.reshape(final_attention_weights, (B, V, V))
+
+        final_attention_weights = torch.sum(final_attention_weights, 1)
+
+        output = torch.mul(aux.squeeze(), final_attention_weights.unsqueeze(-1))
+
+        output = torch.sum(output, 1)
+
+        return output.squeeze(), final_attention_weights
+
+
 class TransformerAggregate(nn.Module):
     def __init__(self, model, feat_dim, num_layers=1, num_heads=4, lifting_net=nn.Sequential()):
         super().__init__()
