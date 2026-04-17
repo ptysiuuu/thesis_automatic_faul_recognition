d459ba51d7ccf2e77bd1d2ca0e6cd8c39d2b25e9
Adam PC <xadirpl@gmail.com>
2026-04-17 19:30:22 +0200

    new script

--- VAR-AI/train.sh
+++ VAR-AI/train.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=VAR-AI_train
+#SBATCH --job-name=VAR-AI_VideoMAE
 #SBATCH --partition=plgrid-gpu-a100
 #SBATCH --account=plggolemml26-gpu-a100
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128G
-#SBATCH --time=12:00:00
-#SBATCH --output=VAR-AI_1
+#SBATCH --time=16:00:00
+#SBATCH --output=VAR-AI_VideoMAE_%j.out
+#SBATCH --error=VAR-AI_VideoMAE_%j.err
 
 source /net/people/plgrid/plgaszos/miniconda3/etc/profile.d/conda.sh
 conda activate vars
 source venv/bin/activate
 
-python main.py --net_name r2plus1d_18 --rule_weight 0.05
+pip install transformers --quiet
+
+python main.py
